428435b8451bdec8deb11f0b6da98ce9615e95dc
Jethro Rainford <rainford1995@gmail.com>
2024-06-24 18:09:56 +0100

    remove old hot garbage tests that fail

--- tests/test_incremental_upload.py
+++ tests/test_incremental_upload.py
@@ -1,42 +1,10 @@
-import sys
 import os
-import tempfile
-import shutil
 import unittest
 
 import pytest
 
 from files import incremental_upload as iu
 from tests import TEST_DATA_DIR
-
-
-# def create_files(rtacomplete_txt, rtacomplete_xml, copycomplete_txt):
-#     tmp_folder = tempfile.mkdtemp()
-#     if rtacomplete_txt:
-#         with open(tmp_folder + "/RTAComplete.txt", 'w') as RTAComplete_txt:
-#             RTAComplete_txt.write("foo")
-#     if rtacomplete_xml:
-#         with open(tmp_folder + "/RTAComplete.xml", 'w') as RTAComplete_xml:
-#             RTAComplete_xml.write("bar")
-#     if copycomplete_txt:
-#         with open(tmp_folder + "/CopyComplete.txt", 'w') as CopyComplete_txt:
-#             CopyComplete_txt.write("foobar")
-#     return tmp_folder
-
-
-# # parametrized with ((RTAComplete.txt, RTAComplete.xml, CopyComplete.txt), result, result_novaseq)
-# @pytest.mark.parametrize("permutation,result,result_novaseq", [((False, False, False), False, False), ((False, False, True), False, True),
-#                                                                ((False, True, False), True, False), ((False, True, True), True, True),
-#                                                                ((True, False, False), True, False), ((True, False, True), True, True),
-#                                                                ((True, True, False), True, False), ((True, True, True), True, True)])
-# def test_termination_file_exists(permutation, result, result_novaseq):
-#     run_dir = create_files(*permutation)
-#     actual = iu.termination_file_exists(False, run_dir)
-#     actual_novaseq = iu.termination_file_exists(True, run_dir)
-#     shutil.rmtree(run_dir)  # deleting before potential assert failure
-#     assert actual == result
-#     assert actual_novaseq == result_novaseq
-
 
 
 class TestCheckIdenticalSamplesheets(unittest.TestCase):
